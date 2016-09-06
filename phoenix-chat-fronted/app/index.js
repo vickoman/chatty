d90a4054b82b75109cc95cef2f3b30d93d24f4c8
Victor Manunel <vicmandlagasca@gmail.com>
2016-09-06 14:24:16 -0500

    Views and Routes

--- phoenix-chat-fronted/app/index.js
+++ phoenix-chat-fronted/app/index.js
@@ -1,13 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { Router, Route, IndexRoute, hashHistory } from "react-router" 
 
-class HelloWorld extends React.Component {
-  render() {
-    return (<div>Hello World vickoman Batiful web!</div>)
-  }
-}
+import Home from "./components/Home"
+import Settings from "./components/Settings"
+
+
+const App = props => (<div>{props.children}</div>)
 
 ReactDOM.render(
-  <HelloWorld />,
-  document.getElementById("root")
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+      <Route path="settings" component={Settings} />
+    </Route>
+  </Router>, document.getElementById("root")
 )
